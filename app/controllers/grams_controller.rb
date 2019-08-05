fd9b56c52932b7ef40acc02369358fa9d6e02811
Jean Velonis <jean.velonis@gmail.com>
2019-08-05 22:10:36 +0000

    picture

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,27 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :update]
+
+  def index
+    @grams = Gram.all
+  end 
+
+  def new
+    @gram = Gram.new
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+    redirect_to root_path
+
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
@@ -7,50 +29,34 @@ class GramsController < ApplicationController
     return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
-  end
-
-  def update
-    @gram = Gram.find_by_id(parmas[:id])
-    return render_not_found if @gram.blank?
-    return render_not_found(:forbidden) if @gram.user != current_user
-  end
-
-  def new
-    @gram = Gram.new
-  end
-
-  def index
-  end
-
-  def show
-    @gram = Gram.find_by_id(params[:id])
-      render plain: 'Not Found :(' if @gram.blank?
-    end
-  end
+  end 
 
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
     return render_not_found(:forbidden) if @gram.user != current_user
-  end
+  end 
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end 
 
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
-      redirect_to root_path
+   redirect_to root_path
+
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titlesize} :(", status: status 
-  end
-
-end
+end 
